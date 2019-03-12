45df3e3c1abcf9791e5b76d41db423236c3646c7
Peter <golishevskiypetro@ukr.net>
2019-03-12 14:13:36 +0200

    Initial Commit

---
+++ FIXER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FIXER
+//
+//  Created by Per Pert on 3/12/19.
+//  Copyright © 2019 Petro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
